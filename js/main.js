dbf97232f7c0ec3a13a41364b159bb50750ddcef
Asif Ashadullah <156141923+asha0012-asif@users.noreply.github.com>
2024-06-07 23:39:13 -0400

    changed hash values in url and accordingly changed display| incomplete due to some issues with popstate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/* ISSUES:
+- historyAPI; displays only the image when clicking forward button (correct behaviour), displays all images when clicking backwards even though a hash value is present in the url 
+- historyAPI; displaying all images works when clicking forward button but shows nothing when clicking backwards button
+*/
+
 const APP = {
     BASE_URL: null,
     cards: null,
@@ -6,11 +11,16 @@ const APP = {
         APP.BASE_URL = "https://random-data-api.com/api/v2/users?size=20";
 
         APP.cards = document.getElementById("cards");
+        
+        APP.checkState();
 
         // register the service worker and add 
         // message event listener
 
         // listen for navigation popstate event
+
+        // popstate is triggered based on the history of the page (back & forward button, works with links changing the state of the page)
+        window.addEventListener("popstate", APP.checkState);
 
         // get the data for the page
         APP.getData();
@@ -21,7 +31,24 @@ const APP = {
 
     registerSW: () => {},
 
-    checkState: () => {},
+    checkState: () => {
+        console.log("\nChecking State\n");
+
+        console.log(location.hash);
+        console.log(history.state);
+        APP.cards.innerHTML = "";
+
+        if (location.hash !== "") {
+            console.log("Hash");
+            const card = APP.showOneCard(history.state);
+            card.setAttribute("data-uid", location.hash.replace("#", ""));
+
+            APP.cards.append(card);
+        } else {
+            console.log("Default");
+        }
+
+    },
 
     getData: async () => {
         console.log("\nGetting Data\n");
@@ -45,6 +72,8 @@ const APP = {
                     avatar
                 }
             });
+
+            // cache newUsers as users.json file
 
             APP.showCards(newUsers);
         } catch (err) {
@@ -89,6 +118,21 @@ const APP = {
 
         const card = ev.target.closest(".card");
         console.log(card);
+
+        const user = {
+            email: card.querySelector(".card__email").textContent,
+            full_name: card.querySelector(".card__full-name").textContent,
+            avatar: card.querySelector(".card__avatar").getAttribute("src")
+        }
+        
+        // hashchange is triggered based on when the hash route is changed (ex. from "#" to "#blah")
+        const hash = `#${card.getAttribute("data-uid")}`;
+        document.title = hash;
+        console.log(hash);
+
+        // change the hash value in the URL
+        history.pushState(user, "", `${location.href}${hash}`);
+        APP.checkState();
     },
 
     sendMessageToSW: () => {},
